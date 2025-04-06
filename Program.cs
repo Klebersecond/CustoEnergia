9d13620695d3268591c1bae5dadd394ff78629a5
Kleber Neves <klebernssii@gmail.com>
2025-04-06 16:52:35 -0300

    Finaliza progama

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿double h, kWhm, cm;
+
+Console.Clear();
+
+Console.Write("Por quanto tempo o seu aparelho fica ligado por dia (h)? : ");
+h = double.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Qual o consumo de energia do seu aparelho em (kwh/m) ? : ");
+kWhm = double.Parse(Console.ReadLine()!);
+
+cm = h * kWhm * 0.62258;
+
+Console.Write($"{cm:C1}");
